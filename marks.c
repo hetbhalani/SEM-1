1f5631d68332ac3852576ea1ee2d086d09a54ad3
Het Bhalani <140876938+hetbhalani@users.noreply.github.com>
2023-10-02 18:19:46 +0530

    Update marks.c

--- marks.c
+++ marks.c
@@ -1,3 +1,7 @@
+/*6A(5) Read marks of five subjects. Calculate percentage and print class accordingly. Fail below 35, Pass Class
+between 36 to 45, Second Class between 46 to 60, First Class between 61 to 70, Distinction if more than 
+70 */
+
 #include<stdio.h>
 
 void main(){
